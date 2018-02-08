3fa6bb813c88d92009e1563cc0d48076b2d4f018
apluta1 <apluta1@users.noreply.github.com>
2018-02-08 11:53:45 -0600

    cool

---
+++ js/code03.js
@@ -0,0 +1,5 @@
+let projectId = 1
+let projectTitle = "Earthquake Map"
+let thumnail = true
+let projectTitles = ["Earthquake Map", "Map of Earthquakes of Magnitude 4.5 or greater"]
+console.log(projectId)
