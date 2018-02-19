d20d97218d7d6abd27f79da7623885c2023e07da
apluta1 <apluta1@users.noreply.github.com>
2018-02-19 10:27:59 -0600

    yed

--- js/code03.js
+++ js/code03.js
@@ -1,5 +1,14 @@
 let projectId = 1
-let projectTitle = "Earthquake Map"
-let thumnail = true
-let projectTitles = ["Earthquake Map", "Map of Earthquakes of Magnitude 4.5 or greater"]
-console.log(projectId)
+let projectUrl = 'https://www.arcgis.com/apps/View/index.html?appid=bda06301dba24bb8b6e77c106a06b290&extent=-181.4062,-71.7006,178.5937,87.0687'
+let projectTitle = 'Map of Recent Earthquakes'
+let projectDesc = 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 2.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS.'
+let projectThumb = true
+let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
+
+
+console.log('ID: ' + projectId)
+console.log('URL: ' + projectUrl)
+console.log('Title: ' + projectTitle)
+console.log('Description: ' + projectDesc)
+console.log('Has thumbnail? ' + projectThumb)
+console.log('Map extent: ' + projectMapExtent)
