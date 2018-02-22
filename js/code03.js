9f39143800a5609c1221b4050bc0c6ef026ee552
apluta1 <apluta1@users.noreply.github.com>
2018-02-22 11:27:10 -0600

    codeassignment4

--- js/code03.js
+++ js/code03.js
@@ -3,8 +3,33 @@ let projectUrl = 'https://www.arcgis.com/apps/View/index.html?appid=bda06301dba2
 let projectTitle = 'Map of Recent Earthquakes'
 let projectDesc = 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 4.5 or greater that have occurred in the last 7 days.'
 let projectThumb = true
-let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
+let projectKeywords = ['Earthquakes', 'worldwide','ArcGIS']
 
+let project1Array = [
+  1,
+  'https://www.arcgis.com/apps/View/index.html?appid=bda06301dba24bb8b6e77c106a06b290&extent=-181.4062,-71.7006,178.5937,87.0687',
+  'Map of Recent Earthquakes',
+  'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 4.5 or greater that have occurred in the last 7 days.',
+  true,
+  ['Earthquakes', 'worldwide','ArcGIS'],
+]
+
+let projectObject = {
+id: 1,
+url: 'https://www.arcgis.com/apps/View/index.html?appid=bda06301dba24bb8b6e77c106a06b290&extent=-181.4062,-71.7006,178.5937,87.0687',
+title: 'Map of Recent Earthquakes',
+desc: 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 4.5 or greater that have occurred in the last 7 days.',
+thumb: true,
+keywords: [
+ 'Earthquakes',
+ 'worldwide',
+ 'ArcGIS'
+]
+}
+  
+console.log(projectArray[2])
+
+console.log(projectObject.title)
 
 console.log('ID: ' + projectId)
 console.log('URL: ' + projectUrl)
