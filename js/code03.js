fd705a0a55d6ffd984e00701318b10a85b90f9d3
apluta1 <apluta1@users.noreply.github.com>
2018-02-22 11:42:03 -0600

    ok

--- js/code03.js
+++ js/code03.js
@@ -1,4 +1,4 @@
-let projectId = 1
+let projectId = 2
 let projectUrl = 'https://www.arcgis.com/apps/View/index.html?appid=bda06301dba24bb8b6e77c106a06b290&extent=-181.4062,-71.7006,178.5937,87.0687'
 let projectTitle = 'Map of Recent Earthquakes'
 let projectDesc = 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 4.5 or greater that have occurred in the last 7 days.'
--- js/code04.js
+++ js/code04.js
@@ -1,5 +1,5 @@
-let project1Array = [
-  1,
+let projectArray = [
+  2,
   'https://www.arcgis.com/apps/View/index.html?appid=bda06301dba24bb8b6e77c106a06b290&extent=-181.4062,-71.7006,178.5937,87.0687',
   'Map of Recent Earthquakes',
   'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 4.5 or greater that have occurred in the last 7 days.',
@@ -8,7 +8,7 @@ let project1Array = [
 ]
 
 let projectObject = {
-id: 1,
+id: 2,
 url: 'https://www.arcgis.com/apps/View/index.html?appid=bda06301dba24bb8b6e77c106a06b290&extent=-181.4062,-71.7006,178.5937,87.0687',
 title: 'Map of Recent Earthquakes',
 desc: 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 4.5 or greater that have occurred in the last 7 days.',
