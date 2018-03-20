47eb914ed325551517022365ccf062dfae640e59
apluta1 <apluta1@users.noreply.github.com>
2018-03-20 11:22:12 -0500

    ok

--- js/codeassignment-06.js
+++ js/codeassignment-06.js
@@ -49,14 +49,16 @@ projectData[4] = {
 
 for (let i = 0; i < projectData.length; i++) {
   createTitle (projectData[i].title)
-  if (projectData[i].thumb === true) {
-    console.log('images/ss-project-' + projectData[i].id + '.png')
-  } else {
-    console.log('images/no-preview.png')
-  }
+  createThumbnail (projectData[i].thumb, projectData[i].id  )
 }
 
 function createTitle (title) {
   console .log('Title' + title)
 }
-function createThumbnail () {}
+function createThumbnail (thumb, id) {
+  if ( thumb === true) {
+    console.log('images/ss-project-' + id + '.png')
+  } else {
+    console.log('images/no-preview.png')
+  }
+}
