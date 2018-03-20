b06c12959bba3ebb7b9bb46bf0ad6d7c7e0b0a6c
apluta1 <apluta1@users.noreply.github.com>
2018-03-20 11:34:25 -0500

    update

--- js/codeassignment-07.js
+++ js/codeassignment-07.js
@@ -51,14 +51,3 @@ for (let i = 0; i < projectData.length; i++) {
   createTitle (projectData[i].title)
   createThumbnail (projectData[i].thumb, projectData[i].id  )
 }
-
-function createTitle (title) {
-  console .log('Title: ' + title)
-}
-function createThumbnail (thumb, id) {
-  if ( thumb === true) {
-    console.log('images/ss-project-' + id + '.png')
-  } else {
-    console.log('images/no-preview.png')
-  }
-}
