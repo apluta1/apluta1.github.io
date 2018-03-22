a72f61af641a12f863a6430fe36944fa26a454d6
apluta1 <apluta1@users.noreply.github.com>
2018-03-22 11:24:00 -0500

    this might not work

--- Map/webmap-assignment-01.js
+++ Map/webmap-assignment-01.js
@@ -1,1 +1,2 @@
 let superdupermap = L.map('AlexMap').setView([29.9637528, -90.058966], 13)
+L.tileLayer('https://{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/hybrid.day/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}').addTo(AlexMap)
