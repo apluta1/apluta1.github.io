e9b2d54572bbd13d6c337f47a92c05a94e7bbcde
apluta1 <apluta1@users.noreply.github.com>
2018-03-22 11:37:50 -0500

    ok

--- Map/webmap-assignment-01.js
+++ Map/webmap-assignment-01.js
@@ -1,2 +1,15 @@
 let superdupermap = L.map('AlexMap').setView([29.9637528, -90.058966], 13)
-L.tileLayer('https://{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/hybrid.day/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}').addTo(AlexMap)
+
+var HERE_hybridDay = L.tileLayer('https://{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/hybrid.day/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}', {
+	attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
+	subdomains: '1234',
+	mapID: 'newest',
+	app_id: '<your app_id>',
+	app_code: '<your app_code>',
+	base: 'aerial',
+	maxZoom: 20,
+	type: 'maptile',
+	language: 'eng',
+	format: 'png8',
+	size: '256'
+}).addTo(AlexMap)
