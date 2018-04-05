b5dd4e325a75282b125cba54a7e057bd8afad22a
apluta1 <apluta1@users.noreply.github.com>
2018-04-05 11:37:31 -0500

    ok

--- Map/webmap-assignment-04.js
+++ Map/webmap-assignment-04.js
@@ -1,8 +1,8 @@
-let mymap = L.map('map4').setView([29.9637528, -90.058966], 4)
+let mymap = L.map('map4').setView([38, -98], 4)
 basemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'
 L.titleLayer(basemapUrl).addTo(mymap)
 
-myGeojsonStyle = function () {
+myGeojsonStyle = function (state) {
   LETAGE = state.properties.MED_AGE
   let stateColor = 'Olive'
   if (age < 38) { stateColor = 'Green'}
@@ -12,7 +12,13 @@ myGeojsonStyle = function () {
   }
   return myStyle
 }
+function createPopup (state, statelayer){
+  let name = state.properties.STATE_NAME
+  let age = state.properties.MED_AGE
+  statelayer.bindPopup('Median age of' + name :':' + age + '<br>National average:38')
+}
 myGeojsonOptions = {
   stle: myGeojsonStyle
+  onEachFeature: createPopup
 }
 L.geoJSON(stateDemographics, myGeojsonOptions).addTo(mymap)
