feadc209f38f56f8f271fb6bf044beeeb244a3b4
apluta1 <apluta1@users.noreply.github.com>
2018-04-18 11:30:05 -0500

    ok

--- map2/webmap-assignment-03.js
+++ map2/webmap-assignment-03.js
@@ -1,9 +1,9 @@
-let theworld = L.map('AlexMapmap').setView([29.951105, -90.081227], 13)
+let superdupermap = L.map('AlexMap').setView([29.951105, -90.081227], 13)
 
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(theworld)
 
-let myredpin = L.icon({
-  iconUrl: 'redpin.png',
+let icon = L.icon({
+  iconUrl: 'thumbtack.png',
   iconSize: [38, 95], // size of the icon
   iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
   popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
@@ -12,8 +12,9 @@ let myredpin = L.icon({
 let mymarker = L.marker([29.951105, -90.081227], {icon: myredpin}).addTo(theworld);
 
 let mypolystyle = {
-  color: 'blue',
-  fillColor: 'yellow'
+  color: 'yellow',
+  fillColor: 'red'
+  fillOpacity: 0.4
 }
 
 let mypolycoords = [
@@ -21,4 +22,4 @@ let mypolycoords = [
   [29.959582, -90.075734],
   [29.944449, -90.076935]]
 
-let myPolygon = L.polygon(mypolycoords, mypolystyle).addTo(theworld)
+let myPolygon = L.polygon(mypolycoords, mypolystyle).addTo(superdupermap)
