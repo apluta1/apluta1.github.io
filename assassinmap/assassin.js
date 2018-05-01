a27c68cd39cc7b0266e13c991e646e6402995855
apluta1 <apluta1@users.noreply.github.com>
2018-05-01 11:46:28 -0500

    Neat

---
+++ assassinmap/assassin.js
@@ -0,0 +1,11 @@
+let mapOptions = {
+  center: [1500, 843],
+  zoom: -2,
+  minZoom: -2,
+  maxZoom: 1,
+  crs: L.CRS.Simple,
+  maxBoundsViscosity: 1.0
+}
+let map = L.map('GimmeTheLoot', mapOptions)
+let imageDimensions = [[0, 0], [3000, 1686]]
+L.imageOverlay('OriginsBasemap.jpg', imageDimensions).addTo(map)
